b98b670af32a539459155e56e1cfeac9a7679359
AuroraSauces <ponxase@gmail.com>
2024-11-29 16:40:14 +0700

    updet api

--- src/api/config/database.php
+++ src/api/config/database.php
@@ -1,21 +1,17 @@
 <?php
-class Database {
-    private $host = 'localhost';
-    private $username = 'root';
-    private $password = '';
-    private $database = 'SeeJTI';
-    public $koneksi;
+function connectDatabase() {
+    $host = 'localhost';
+    $dbname = 'seejti';
+    $username = 'sa';
+    $password = 'seejti';
 
-    public function __construct() {
-        $this->koneksi = new mysqli($this->host, $this->username, $this->password, $this->database);
-        
-        if ($this->koneksi->connect_error) {
-            die("Koneksi gagal: " . $this->koneksi->connect_error);
-        }
-    }
-
-    public function getKoneksi() {
-        return $this->koneksi;
+    try {
+        $pdo = new PDO("sqlsrv:Server=$host;Database=$dbname", $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    } catch (PDOException $e) {
+        echo "Koneksi ke database gagal: " . $e->getMessage();
+        return null;
     }
 }
 ?>
