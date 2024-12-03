1e702b9488e7d350636411e15578a55e7a8b2648
Mitsuha Knoyan <knoyan@knoyan.local>
2024-12-03 13:02:35 +0700

    update role

---
+++ src/controller/Role.ts
@@ -0,0 +1,13 @@
+export default class Role {
+  role: string = '';
+
+
+  constructor() {
+    this.updateRole();
+  }
+
+  updateRole() {
+    // kalau mau ganti admin dan user disini
+    this.role = 'ketua';
+  }
+}
