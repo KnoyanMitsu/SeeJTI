27ffbefceb3ef7b1a78a7fbf2ecae99106bc1bf5
Mitsuha Knoyan <knoyan@knoyan.local>
2024-12-03 13:02:35 +0700

    update role

---
+++ src/widget/NavClassroom.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex">
+
+  </div>
+  <div class="flex text-white font-bold items-center text-center mb-3 mt-3">
+    <ul class="font-semibold flex col gap-16">
+      <li class="flex items-center gap-2">
+          <vs-icon icon="home"></vs-icon>
+          <router-link to="/">Home</router-link>
+      </li>
+      <li class="flex items-center gap-2">
+        <vs-icon icon="book"></vs-icon>
+        <router-link to="/jadwal/kelas">Jadwal Perkuliahan</router-link>
+      </li>
+      <li class="flex items-center gap-2">
+          <vs-icon icon="domain"></vs-icon>
+          <router-link to="/jadwal/kosong">Ruang kosong</router-link>
+      </li>
+    </ul>
+  </div>
+  <div class="flex"></div>
+</template>
--- src/widget/NavWidget.vue
+++ src/widget/NavWidget.vue
@@ -1,3 +1,21 @@
+<script>
+import Role from '@/controller/Role';
+
+export default {
+  data() {
+    return {
+      role: '',
+    };
+  },
+  mounted() {
+    const roleInstance = new Role();
+    setInterval(() => {
+      this.role = roleInstance.role;
+    })
+  }
+};
+</script>
+
 <template>
   <div class="grid lg:grid-cols-2 gap-4 mx-auto md:grid-cols-2">
     <router-link to="/jadwal/kelas" class="w-60 bg-white rounded-md shadow-md">
@@ -6,7 +24,7 @@
           <vs-icon icon="book"></vs-icon>
         </div>
 
-        <p class="text-2xl font-semibold text-gray-800">
+      <p class="text-2xl font-semibold text-gray-800">
           Jadwal <br />
           Perkuliahan
         </p>
@@ -18,21 +36,23 @@
       </div>
     </router-link>
 
-    <div class="w-60 bg-white rounded-lg shadow-md">
-      <div class="flex flex-col ml-7">
-        <div class="text-gray-700 mb-2 mt-7">
-          <vs-icon icon="building"></vs-icon>
+    <router-link to="jadwal/kosong">
+      <div v-if="role === 'ketua'" class="w-60 bg-white rounded-lg shadow-md">
+        <div class="flex flex-col ml-7">
+          <div class="text-gray-700 mb-2 mt-7">
+            <vs-icon icon="building"></vs-icon>
+          </div>
+          <p class="text-2xl font-semibold text-gray-800">
+            Ruang <br />
+            Kosong
+          </p>
         </div>
-        <p class="text-2xl font-semibold text-gray-800">
-          Ruang <br />
-          Kosong
-        </p>
+        <div class="flex mt-7">
+          <div class="h-1 w-1/3 bg-blue-900"></div>
+          <div class="h-1 w-1/3 bg-orange-500"></div>
+          <div class="h-1 w-1/3 bg-yellow-500"></div>
+        </div>
       </div>
-      <div class="flex mt-7">
-        <div class="h-1 w-1/3 bg-blue-900"></div>
-        <div class="h-1 w-1/3 bg-orange-500"></div>
-        <div class="h-1 w-1/3 bg-yellow-500"></div>
-      </div>
-    </div>
+    </router-link>
   </div>
 </template>
