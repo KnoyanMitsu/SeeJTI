d486c68b979fb503a29109061c5d22f479eb7de0
Knoyan Mitsu <yanappuowo@yandex.com>
2024-12-04 08:57:30 +0700

    fix jadwal kosong hide on mahasiswa

--- src/widget/NavWidget.vue
+++ src/widget/NavWidget.vue
@@ -36,23 +36,26 @@ export default {
       </div>
     </router-link>
 
-    <router-link to="jadwal/kosong">
-      <div v-if="role === 'ketua'" class="w-60 bg-white rounded-lg shadow-md">
-        <div class="flex flex-col ml-7">
-          <div class="text-gray-700 mb-2 mt-7">
-            <vs-icon icon="building"></vs-icon>
+    <div>
+
+      <router-link to="jadwal/kosong">
+        <div class="w-60 bg-white rounded-lg shadow-md">
+          <div class="flex flex-col ml-7">
+            <div class="text-gray-700 mb-2 mt-7">
+              <vs-icon icon="building"></vs-icon>
+            </div>
+            <p class="text-2xl font-semibold text-gray-800">
+              Ruang <br />
+              Kosong
+            </p>
           </div>
-          <p class="text-2xl font-semibold text-gray-800">
-            Ruang <br />
-            Kosong
-          </p>
+          <div class="flex mt-7">
+            <div class="h-1 w-1/3 bg-blue-900"></div>
+            <div class="h-1 w-1/3 bg-orange-500"></div>
+            <div class="h-1 w-1/3 bg-yellow-500"></div>
+          </div>
         </div>
-        <div class="flex mt-7">
-          <div class="h-1 w-1/3 bg-blue-900"></div>
-          <div class="h-1 w-1/3 bg-orange-500"></div>
-          <div class="h-1 w-1/3 bg-yellow-500"></div>
-        </div>
-      </div>
-    </router-link>
+      </router-link>
+    </div>
   </div>
 </template>
