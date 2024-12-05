a6868eb10891be30daf952d606b2175f5746c2b6
Knoyan Mitsu <yanappuowo@yandex.com>
2024-12-05 10:39:49 +0700

    navbar test

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -2,8 +2,56 @@
 import JTI from '@/assets/Logo/jti_polinema 3.png'
 </script>
 
+<script>
+import axios from '../api/api'
+let cachedUser = null
+export default {
+  data() {
+    return {
+      user: [], // Data jadwal
+    }
+  },
+  methods: {
+    async fetchSchedules() {
+      const maxRetries = 10
+      let attempt = 0
+      let success = false
+      // Jika data sudah ada di cache, gunakan cache
+      if (cachedUser) {
+        this.setUserData(cachedSchedules)
+        return
+      }
+
+      while (attempt < maxRetries && !success) {
+        try {
+          const response = await axios.get('http://localhost:8000/user.php')
+
+          if (response.data && response.data.classes) {
+            cachedUser = response.data.users // Simpan data ke cache
+            this.setUserData(cachedUser)
+            this.success = true
+            success = true
+          } else {
+            console.error('Invalid API Response:', response.data)
+            throw new Error('Invalid response')
+          }
+        } catch (error) {
+          attempt++
+          console.error(`Error fetching schedules (attempt ${attempt}):`, error)
+          if (attempt >= maxRetries) {
+            console.error('Max retries reached. Unable to fetch schedules.')
+          }
+        }
+      }
+    },
+    setUserData(data) {
+      this.user = data
+    },
+  },
+}
+</script>
 <template>
-  <nav class="flex items-center justify-between flex-wrap p-6 ">
+  <nav class="flex items-center justify-between flex-wrap p-6">
     <router-link to="/" class="flex items-center flex-shrink-0 text-white mr-6">
       <span class="font-bold text-2xl tracking-tight text-black px-4 py-2"
         >SeeJTI</span
@@ -33,9 +81,11 @@ import JTI from '@/assets/Logo/jti_polinema 3.png'
         />
         <div class="grid ml-3 gap">
           <p class="text-black text-left rounded row-span-3 font-semibold">
-            Tianlu
+            {{ user.name }}
           </p>
-        <p class="text-black/50 text-left rounded text-xs font-medium">9912803174 | TI-YES</p>
+          <p class="text-black/50 text-left rounded text-xs font-medium">
+            {{ user.nim }} | {{ user.class }}
+          </p>
         </div>
       </div>
     </div>
