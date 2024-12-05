e6273a13f0c7a31210d2eebba6cead233be0a508
Knoyan Mitsu <yanappuowo@yandex.com>
2024-12-05 09:37:31 +0700

    j

--- src/views/NoRoomView.vue
+++ src/views/NoRoomView.vue
@@ -7,14 +7,13 @@ import NavClassroom from '@/widget/NavClassroom.vue'
 </script>
 
 <script>
-import { ref } from 'vue'
 import {
   Dialog,
   DialogPanel,
-  DialogTitle,
   TransitionChild,
   TransitionRoot,
 } from '@headlessui/vue'
+import { ref } from 'vue'
 // import { ExclamationTriangleIcon } from '@heroicons/vue/24/outline'
 
 // Kontrol visibilitas dialog
@@ -87,7 +86,6 @@ export default {
     </div>
   </div>
 
-
   <!-- Dialog -->
   <TransitionRoot as="template" :show="open">
     <Dialog class="relative z-10" @close="open = false">
@@ -104,7 +102,9 @@ export default {
       </TransitionChild>
 
       <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
-        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+        <div
+          class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0"
+        >
           <TransitionChild
             as="template"
             enter="ease-out duration-300"
@@ -114,37 +114,36 @@ export default {
             leave-from="opacity-100 translate-y-0 sm:scale-100"
             leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+            <DialogPanel
+              class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg"
+            >
               <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                 <p class="mb-3">Nama Matkul</p>
-                <select name="room"  id="" class="bg-white mb-4 w-full">
+                <select name="room" id="" class="bg-gray-100 rounded-md mb-4 w-full">
                   <option value="sim">Test</option>
                 </select>
                 <p class="mb-3">Nama Ruang</p>
-                <select name="room"  id="" class="bg-white mb-4 w-full">
+                <select name="room" id="" class="bg-gray-100 rounded-md mb-4 w-full">
                   <option value="sim">Test</option>
                 </select>
                 <p class="mb-3">Hari</p>
-                <input type="text" class="bg-white mb-4 w-full">
+                <input type="text" class="bg-gray-100 rounded-md mb-4 w-full" />
                 <p class="mb-3">Jam</p>
-                <input type="time" class="bg-white mb-4 w-full">
+                <div class="flex">
+                  <input type="text" class="bg-gray-100 rounded-md mb-4 w-full mr-3" />
+                  <input type="text" class="bg-gray-100 rounded-md mb-4 w-full" />
+                </div>
+                <p class="mb-3">Kelas</p>
+                <input type="text" class="bg-gray-100 rounded-md mb-4 w-full" />
               </div>
 
-              <div class="bg-white px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+              <div class="bg-white px-4 py-3 sm:flex justify-center sm:px-6">
                 <button
-                  type="button"
-                  class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
-                  @click="open = false"
+                  @click="open = true"
+                  type="submit"
+                  class="px-4 py-2 text-white bg-[#F05529] rounded-full mb-4 hover:bg-[#FEA127]"
                 >
-                  Deactivate
-                </button>
-                <button
-                  type="button"
-                  class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                  @click="open = false"
-                  ref="cancelButtonRef"
-                >
-                  Cancel
+                  Kirim Permintaan
                 </button>
               </div>
             </DialogPanel>
