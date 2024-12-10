9b5f080434bfce7ad4357305a18347ad7ec48e43
ahmadrmdhann <ahmadramadhansqorpio04@gmail.com>
2024-12-10 14:37:07 +0700

    test

--- src/api/config/database.php
+++ src/api/config/database.php
@@ -1,17 +1,5 @@
 <?php
 ini_set('mssql.timeout', 6);
-function checkServer($host, $timeout = 5) {
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $host);
-    curl_setopt($ch, CURLOPT_NOBODY, true);
-    curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $response = curl_exec($ch);
-    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    curl_close($ch);
-    
-    return ($response && $httpCode === 200);
-}
 
 function connectDatabase() {
     $host = '192.168.191.191';
